662904d53e6cbe4ff651850156313d5d9fac3c36
DanGlChris <dagloxkankwanda@gmail.com>
2023-10-31 17:14:54 +0300

    "Initial commit"

---
+++ tests/test_skeleton.py
@@ -0,0 +1,25 @@
+import pytest
+
+from short_activist_predictor.skeleton import fib, main
+
+__author__ = "DanGlChris"
+__copyright__ = "DanGlChris"
+__license__ = "GPL-2.0-only"
+
+
+def test_fib():
+    """API Tests"""
+    assert fib(1) == 1
+    assert fib(2) == 1
+    assert fib(7) == 13
+    with pytest.raises(AssertionError):
+        fib(-10)
+
+
+def test_main(capsys):
+    """CLI Tests"""
+    # capsys is a pytest fixture that allows asserts against stdout/stderr
+    # https://docs.pytest.org/en/stable/capture.html
+    main(["7"])
+    captured = capsys.readouterr()
+    assert "The 7-th Fibonacci number is 13" in captured.out
